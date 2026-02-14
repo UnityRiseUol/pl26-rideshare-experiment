5fb97b48290eceda2eb18cf4f0bcfc362a4ff965
T-Kalv <t.kalvin@outlook.com>
2026-02-14 21:55:23 +0000

    Added camera_Test.py to test Raspberry Pi Camera Module 3 No IR works

---
+++ camera_Test.py
@@ -0,0 +1,19 @@
+# Program:
+# Author:
+# Module:
+# Email:
+# Student Number:
+# -----------------------------------------------------------------------------------------------------------------------------
+# Code
+from picamera2 import Picamera2
+import time
+
+picam2 = Picamera2()
+config = picam2.create_still_configuration(main={"size": (1920, 1080)})
+picam2.configure(config)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test_image.jpg")
+picam2.stop()
+
+print("Image captured successfully")
